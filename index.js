fdc8bf04dbee4ca8ee961ec9942b12c0d9c3c79a
Ananthakrishnan.S <ananthakrishnans.muhamma@gmail.com>
2022-01-16 15:08:40 +0530

    edit & delete function added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const path = require('path')
 const port = 3000
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/countrydb')
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/',(req,res)=>{
     res.redirect('/countries')
@@ -41,6 +43,28 @@ app.get('/countries/:id',async (req,res)=>{
     const country = await Countries.findById(id)
     console.log(country)
     res.render('country-details',{country})
+})
+app.get('/countries/:id/edit',async (req,res)=>{
+    const {id} = req.params
+    const country = await Countries.findById(id)
+    res.render('country-edit',{country})
+})
+app.put('/countries/:id',async (req,res)=>{
+    const {id} = req.params
+    console.log(req.body)
+    const foundCountry = await Countries.findByIdAndUpdate(id,req.body,{runValidators: true})
+    res.redirect(`/countries/${id}`)
+})
+app.get('/countries/:id/delete',async (req,res)=>{
+    const {id} = req.params
+    const country = await Countries.findById(id)
+    res.render('country-delete',{country})
+})
+app.delete('/countries/:id',async (req,res)=>{
+    const {id} = req.params
+    console.log(req.body)
+    const foundCountry = await Countries.findByIdAndDelete(id,req.body)
+    res.redirect(`/countries`)
 })
 app.listen(port,()=>{
     console.log(`listening to port ${port}`)
